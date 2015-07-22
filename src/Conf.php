f14ea92ac2d3d4156e46620fc752d513db054139
Dinar Garipov <garipov.dinar@gmail.com>
2015-07-22 16:50:41 +0300

    added methods

--- src/Conf.php
+++ src/Conf.php
@@ -3,28 +3,98 @@
 namespace Gaaarfild\LaravelConf;
 
 use Illuminate\Support\Traits\Macroable;
+use Session;
+use Config;
 
+/**
+ * Class Conf
+ * @package Gaaarfild\LaravelConf
+ */
 class Conf
 {
 
     use Macroable;
 
+    /**
+     * In-memory config store
+     *
+     * @var \Illuminate\Support\Collection|null
+     */
+    private $config = null;
 
+    /**
+     * File to save config file
+     *
+     * @var null|string
+     */
+    private $file = null;
+
+    /**
+     * Create new instance of Conf class
+     */
     public function __construct()
     {
-
+        $this->file = storage_path('app/conf.json');
+        if (empty($this->config)) {
+            if (!file_exists($this->file)) {
+                file_put_contents($this->file, json_encode([]));
+            }
+            $this->config = collect(json_decode(file_get_contents($this->file), true));
+        }
     }
 
     /**
-     * Return config value by key
+     * Store config value by key
+     *
+     * @param $key
+     * @param $value
+     */
+    public function set($key, $value)
+    {
+        $this->config->put($key, $value);
+        file_put_contents($this->file, $this->config->toJson());
+    }
+
+    /**
+     * Check existence of the key
+     *
+     * @param $key
+     * @param bool $withFallback
+     * @return string
+     */
+    public function has($key, $withFallback=false)
+    {
+        $has = $this->config->has($hey);
+        if($withFallback) {
+            return $has;
+        } else {
+            if ($has) {
+                return $has;
+            } else {
+                return Config::has($key);
+            }
+        }
+    }
+
+    /**
+     * Get config value by key
      *
      * @param $key
      * @param bool $default
      * @return string
      */
-    public function get($key, $default=false)
+    public function get($key, $default=null)
     {
-        return 'ok';
+        return $this->config->get($key, Config::get($key, $default));
+    }
+
+    /**
+     * Return entire config
+     * @return string
+     */
+    public function all()
+    {
+        return $this->config->all();
     }
 
 }
